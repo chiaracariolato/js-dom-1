413187bf280c8c7efc66e802ea919601b5600d30
chiaracariolato <chiaracariolato@gmail.com>
2026-02-17 18:33:35 +0100

    fix: added mirror image

--- js/script.js
+++ js/script.js
@@ -7,29 +7,33 @@ const img = document.getElementById('pic');
 //   if (button.innerHTML === 'Accendi') {
 //     button.innerHTML = 'Spegni';
 //     img.src = 'img/yellow_lamp.png';
+//     img.style="transform: scaleX(-1)";
 //     img.alt = 'Lampadina accesa'
 //   } else {
 //     button.innerHTML = 'Accendi';
 //     img.src = 'img/white_lamp.png';
+//     img.style="";
 //     img.alt = 'Lampadina spenta'
 //   }
 // }
 // );
 
 // Alternativa 1 con textContent
-// button.addEventListener('click', 
-// function () {
-//   if (button.textContent === 'Accendi') {
-//     button.textContent = 'Spegni';
-//     img.src = 'img/yellow_lamp.png';
-//     img.alt = 'Lampadina accesa'
-//   } else {
-//     button.textContent = 'Accendi';
-//     img.src = 'img/white_lamp.png';
-//     img.alt = 'Lampadina spenta'
-//   }
-// }
-// );
+button.addEventListener('click', 
+function () {
+  if (button.textContent === 'Accendi') {
+    button.textContent = 'Spegni';
+    img.src = 'img/yellow_lamp.png';
+    img.style="transform: scaleX(-1)";
+    img.alt = 'Lampadina accesa'
+  } else {
+    button.textContent = 'Accendi';
+    img.src = 'img/white_lamp.png';
+    img.style="";
+    img.alt = 'Lampadina spenta'
+  }
+}
+);
 
 // Alternativa 2 con string.includes()
 // button.addEventListener('click', 
@@ -51,21 +55,21 @@ const img = document.getElementById('pic');
 
 
 // Versione ottimizzata
-const isOn = true;
+// const isOn = true;
 
-button.addEventListener('click', accendi);
+// button.addEventListener('click', accendi);
 
-function accendi() {
-    let innerHTML = 'Accendi';
-    let source = 'img/white_lamp.png';
-    let alt = 'Lampadina spenta';
-    if (isOn) {
-        innerHTML = 'Spegni';
-        source = 'img/yellow_lamp.png';
-        alt = 'Lampadina accesa' 
-    }
-    isOn = !isOn;
-    button.innerHTML = innerHTML;
-    img.src = source;
-    img.alt = alt;
-};
+// function accendi() {
+//     let innerHTML = 'Accendi';
+//     let source = 'img/white_lamp.png';
+//     let alt = 'Lampadina spenta';
+//     if (isOn) {
+//         innerHTML = 'Spegni';
+//         source = 'img/yellow_lamp.png';
+//         alt = 'Lampadina accesa' 
+//     }
+//     isOn = !isOn;
+//     button.innerHTML = innerHTML;
+//     img.src = source;
+//     img.alt = alt;
+// };
